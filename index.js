2f3dee3a8c160ef1f632c8602ca590aa5b09cc8e
Alisa <alisakovalhome@mail.ru>
2023-04-11 22:45:31 +0300

    Add correct solution

--- index.js
+++ index.js
@@ -1,8 +1,43 @@
 // Этот метод должен искать элемент в массиве и возвращать его индекс или -1, если элемент не находится в массиве.
 // Например, если ты ищешь G в массиве [A, B, C, D, E, F, G, H], твой метод должен возвращать 6. Если ты ищешь X, он должен возвращать -1.
 
-function binarySearch (){
+function binarySearch(arr, item) {
+    arr.sort((a, b) => a - b)
+    let start = 0
+    let end = arr.length - 1
+    return binarySearchExecution(arr, item, start, end)
+}
 
+function binarySearchExecution(arr, item, start, end) {
+
+    let middle = Math.floor((start + end) / 2)
+
+    if (end < start) return -1
+    if (item == arr[middle]) return middle
+    if (item < arr[middle]) return binarySearchExecution(arr, item, start, middle - 1)
+    if (item > arr[middle]) return binarySearchExecution(arr, item, middle + 1, end)
 }
 
 module.exports = binarySearch
+
+
+
+
+//итерационное решение (для меня)
+// const binarySearchByIteration = (arr, item) => {
+//     let start = 0
+//     let end = arr.length - 1
+
+//     while (start <= end) {
+//         let middle = Math.floor((start + end) / 2)
+//         if (arr[middle] == item) return middle
+
+//         if (arr[middle] > item) {
+//             end = middle - 1
+//         } else if (arr[middle] < item) {
+//             start = middle + 1
+//         }
+//     }
+//     return - 1 
+
+
--- spec/binarysearch.spec.js
+++ spec/binarysearch.spec.js
@@ -1,13 +1,28 @@
 const binarySearch = require('../index')
 
 test('Функция должна возвращать индекс элемента', () => {
-    
+    const arr = ['A', 'B', 'D', 'C', 'E', 'F', 'G', 'H']
+    const arr1 = [1, 2, 5, 4, 3]
+    const arr2 = ['A', 'B']
+    const arr3 = ['A']
+
+    expect(binarySearch(arr, 'G')).toEqual(6)
+    expect(binarySearch(arr, 'B')).toEqual(1)
+    expect(binarySearch(arr1, 3)).toEqual(2)
+    expect(binarySearch(arr2, 'B')).toEqual(1)
+    expect(binarySearch(arr3, 'A')).toEqual(0)
 })
 
 test('Функция должна быть определена', () => {
-    
+    const arr = ['A', 'B', 'D', 'C', 'E', 'F', 'G', 'H']
+    expect(binarySearch(arr, 'G')).toBeDefined()
 })
 
 test('Функция должна возвращать -1, если элемента не существует в данном массиве', () => {
-    
+    const arr = ['A', 'B', 'D', 'C', 'E', 'F', 'G', 'H']
+    const arr1 = []
+    const arr2 = ['A']
+    expect(binarySearch(arr, 'T')).toEqual(-1)
+    expect(binarySearch(arr1, 'B')).toEqual(-1)
+    expect(binarySearch(arr2, 'A')).toEqual(0)
 })
